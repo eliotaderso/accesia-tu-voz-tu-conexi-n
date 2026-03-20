7e83d62e621727c8e50f159b8a3e7ef6fe5e58f2
eliotaderso <talanparker6666@gmail.com>
2026-03-20 10:32:17 +0100

    Remove comment from base URL in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/accesia-tu-voz-tu-conexi-in/', // 👈 ESTO ES LA CLAVE
+  base: '/accesia-tu-voz-tu-conexi-in/', // 
 })
